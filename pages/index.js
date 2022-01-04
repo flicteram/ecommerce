0d19e04037f34be7c6fd4852336a07268337e0b1
Flicter Alexandru <flicteralexandru@gmail.com>
2022-01-04 22:01:50 +0200

    Added header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div>
-      <h1>Ecommerce</h1>
+      <Header/>
     </div>
   )
 }
