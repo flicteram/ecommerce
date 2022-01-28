7aa5854fce55f2616b3ffdae29300c587f304ba8
Flicter Alexandru <flicteralexandru@gmail.com>
2022-01-28 14:28:25 +0200

    Made product modal mobile and desktop responsive

--- components/ModalConfirmation/ModalConfirmation.js
+++ components/ModalConfirmation/ModalConfirmation.js
@@ -5,39 +5,36 @@ import Image from 'next/image'
 
 
 export default function ModalConfirmation({modalIsOpen,setModalIsOpen,product}){
-    const styleModal={
-        overlay:{
-            position:'fixed',
-        },
-        content:{
-            top:'50%',
-            transform:'translateY(-50%)',
-            maxWidth:'1160px',
-            maxHeight:'500px',
-            margin:'0 auto',
-        }
-    }
+
     const handleCloseModal=()=>setModalIsOpen(false)
 
-    console.log(product)
     return(
         <Modal
         isOpen={modalIsOpen}
         onRequestClose={handleCloseModal}
-        style={styleModal}
+        className={styles.Modal}
+        overlayClassName={styles.Overlay}
+        ariaHideApp={false}
         >
         <div>
             <div className={styles.productAddedContainer}>
-                <h5>Product added to your cart</h5>
-                <button onClick={handleCloseModal}><CloseIcon sx={{fontSize:'30px'}}/></button>
+                <h4>Product added to your cart</h4>
+                <button onClick={handleCloseModal}><CloseIcon sx={{fontSize:'35px',color:'rgb(220,230,228)'}}/></button>
             </div>
             <div className={styles.productInfoContainer}>
                 <div className={styles.imageContainer} style={{position:'relative'}}>
                     <Image src={product.images[0]} objectFit='cover' layout='fill'/>
                 </div>
-                <p className={styles.color} style={{backgroundColor:product.color}}></p>
-                <p>{product.name}</p>
-                <p>${product.price}</p>
+                <div>
+                    <p className={styles.name}>{product.name}</p>
+                    <p className={styles.price}>${product.price}</p>
+                    <div className={styles.colorContainer}>
+                        <p className={styles.colorP}>Color:</p>
+                        <p className={styles.color} style={{backgroundColor:product.color}}></p>
+                    </div>
+                    <button className={styles.viewCart}>VIEW CART</button>
+                </div>
+
             </div>
         </div>
         
