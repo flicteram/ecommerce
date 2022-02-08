17f2fff29a37123a052e774b282f302dbd83d5f7
Flicter Alexandru <flicteralexandru@gmail.com>
2022-02-08 22:21:27 +0200

    Add loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,33 @@
 import '../styles/globals.css'
 import {ContextProvider} from '../components/Context/Context'
+import {useRouter} from 'next/router'
+import {useState,useEffect} from 'react'
+import Loader from '../components/Loader/Loader'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    router.events.on("routeChangeError", () => setLoading(true));
+    router.events.on("routeChangeStart", () => setLoading(true));
+    router.events.on("routeChangeComplete", () => setLoading(false));
+
+    return () => {
+      router.events.off("routeChangeError", () => setLoading(true));
+      router.events.off("routeChangeStart", () => setLoading(true));
+      router.events.off("routeChangeComplete", () => setLoading(false));
+    };
+  }, [router.events]);
 
   return(
-    <ContextProvider>
-      <Component {...pageProps} />
-    </ContextProvider>
+    <>
+        {loading&&<Loader/>}
+        <ContextProvider>
+          <Component {...pageProps} />
+        </ContextProvider>
+    </>
+
   )
  
 }
