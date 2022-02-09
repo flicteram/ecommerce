7de42d8002876d798d5873d1604885d078de0ac6
Flicter Alexandru <flicteralexandru@gmail.com>
2022-02-09 12:20:21 +0200

    Remove loader when route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,10 @@
 import '../styles/globals.css'
 import {ContextProvider} from '../components/Context/Context'
-import {useRouter} from 'next/router'
-import {useState,useEffect} from 'react'
-import Loader from '../components/Loader/Loader'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    router.events.on("routeChangeError", () => setLoading(true));
-    router.events.on("routeChangeStart", () => setLoading(true));
-    router.events.on("routeChangeComplete", () => setLoading(false));
-
-    return () => {
-      router.events.off("routeChangeError", () => setLoading(true));
-      router.events.off("routeChangeStart", () => setLoading(true));
-      router.events.off("routeChangeComplete", () => setLoading(false));
-    };
-  }, [router.events]);
 
   return(
     <>
-        {loading&&<Loader/>}
         <ContextProvider>
           <Component {...pageProps} />
         </ContextProvider>
