34e269ad58344c9c76cfa3996c4aa7485f4bf376
Flicter Alexandru <flicteralexandru@gmail.com>
2022-02-09 14:47:10 +0200

    Add home route on click logo

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,13 @@
 import styles from '../../styles/Header.module.css'
 import HeaderLow from './HeaderLow/HeaderLow'
 import HeaderBig from './HeaderBig/HeaderBig'
+import Link from 'next/link'
 
 export default function Header({home, about, products}){
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContainerInner}>
-                <p className={styles.logo}>Comfort<span className={styles.logoZone}>Zone</span></p>
+                <Link href='/'><p className={styles.logo}>Comfort<span className={styles.logoZone}>Zone</span></p></Link>
                 <HeaderLow/>
                 <HeaderBig home={home} about={about} products={products}/>
             </div>
