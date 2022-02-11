a3be5cb1147f2c74c598918883d30dfda543d7ba
Flicter Alexandru <flicteralexandru@gmail.com>
2022-02-11 23:10:07 +0200

    Add loader on click product

--- components/Product/Product.js
+++ components/Product/Product.js
@@ -1,37 +1,39 @@
 import styles from '../../styles/Product.module.css'
 import Image from 'next/image';
+import { useState } from 'react';
 import { useRouter } from 'next/router'
 import { useInView } from 'react-hook-inview'
+import LinearProgress from '@mui/material/LinearProgress';
 
 export default function Product({product,index,id}){
   const router = useRouter()
 
+  const [loading,setLoading]=useState(false)
+
   const [ref, inView] = useInView({unobserveOnEnter:true,threshold:1})
 
-  function prodInView(){
-    if(inView){
-      return styles.productContainerAnim
-    }
-    else{
-      return styles.productContainer
-    }
-  }
+  const prodInView=()=>inView?styles.productContainerAnim:styles.productContainer
+  const handleOnClickProduct=()=>(setLoading(true), router.push(`products/${id}`))
+
     return(
-    <div className={prodInView()} onClick={()=>router.push(`products/${id}`)} ref={ref}>
-        <Image src={product.images[0]}
-         sizes="50vw"
-         priority={index<=10?true:false}
-         width={30} 
-         height={25} 
-         objectFit='cover' 
-         className={styles.image} 
-         objectPosition={'50% 70%'} 
-         layout='responsive'
-         />
-        <div className={styles.productInfo}>
-          <p className={styles.productName}>{product.name}</p>
-          <p className={styles.productPrice}>${product.price}</p>
-        </div>
+    <div>
+    {loading&&<LinearProgress color={'inherit'} sx={{color:'rgb(214, 107, 19)',position:'fixed',top:'0',left:'0',right:'0',zIndex:'1100'}}/>}
+      <div className={prodInView()} onClick={handleOnClickProduct} ref={ref}>
+          <Image src={product.images[0]}
+          sizes="50vw"
+          priority={index<=5?true:false}
+          width={30} 
+          height={25} 
+          objectFit='cover' 
+          className={styles.image} 
+          objectPosition={'50% 70%'} 
+          layout='responsive'
+          />
+          <div className={styles.productInfo}>
+            <p className={styles.productName}>{product.name}</p>
+            <p className={styles.productPrice}>${product.price}</p>
+          </div>
+      </div>
     </div>
     )
 }
